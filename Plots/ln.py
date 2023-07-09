40c571101b2bedf4a6aa9def5a9b95c1c8777639
Yanick Kind <yanick.kind@udo.edu>
2023-07-10 00:47:46 +0200

    lol

---
+++ Plots/ln.py
@@ -0,0 +1,35 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+x = np.linspace(1e-3, 2, 500)
+
+plt.figure()
+
+plt.plot(x, 3/np.log(1+x), color = 'deeppink')
+plt.plot(x, 3/np.sqrt(x), color = 'grey')
+plt.plot(x, 3/x, color = 'grey')
+plt.ylim(1, 20)
+plt.xticks([],[])
+plt.yticks([],[])
+plt.savefig('ln.pdf', bbox_inches = 'tight')
+
+plt.figure()
+
+plt.plot(x, 3/np.log(1+x), color = 'grey')
+plt.plot(x, 3/np.sqrt(x), color = 'deeppink')
+plt.plot(x, 3/x, color = 'grey')
+plt.ylim(1, 30)
+plt.xticks([],[])
+plt.yticks([],[])
+plt.savefig('sqrt.pdf', bbox_inches = 'tight')
+
+plt.figure()
+
+plt.plot(x, 3/np.log(1+x), color = 'grey')
+plt.plot(x, 3/np.sqrt(x), color = 'grey')
+plt.plot(x, 3/x, color = 'deeppink')
+plt.ylim(1, 30)
+plt.xticks()
+plt.xticks([],[])
+plt.yticks([],[])
+plt.savefig('t.pdf', bbox_inches = 'tight')
